dcd6a015a16f5c19c6b447a9751ff11940b79bb2
GaetanDesrues <gaetan.desrues@free.fr>
2019-01-06 19:22:22 +0100

    ok

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ model = UNet(in_channels=1, n_classes=2, padding=True, up_mode='upsample').to(de
 #         print("Attention : le modèle n'existe pas encore et va être créé !")
 
 # Optimisateur pour l'algorithme du gradient
-optim = torch.optim.SGD(model.parameters() , lr=0.1)
+optim = torch.optim.SGD(model.parameters() , lr=0.1, momentum=0.9)
 # optim = torch.optim.Adam(model.parameters() , lr=0.0005)
 
 # Objet représentant les données
