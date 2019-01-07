ed2b0767878622823a33045f0e86d60d8a435daf
Corisande Lamy <corisande.lamy@inria.fr>
2019-01-07 14:09:49 +0100

    test02

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ import torchvision.transforms as transforms
 options = OptionCompilation()
 
 # TensorBoardX pour les visualisations
-writer = SummaryWriter('output/runs/test-09')#exp-29-11-test')
+writer = SummaryWriter('output/runs/test-02')#exp-29-11-test')
 # arg : Rien pour le nom par défaut, comment='txt' pour ajouter un com à la fin
 
 # Charge le fichier de configurations
