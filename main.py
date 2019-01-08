cc7f4bca8fa8cf2bb89ca9b9a445eea4c207fd70
GaetanDesrues <gaetan.desrues@free.fr>
2019-01-08 19:32:08 +0100

    ok

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ import torchvision.transforms as transforms
 options = OptionCompilation()
 
 # TensorBoardX pour les visualisations
-writer = SummaryWriter('output/runs/test-17')#exp-29-11-test')
+writer = SummaryWriter('output/runs/test-18')#exp-29-11-test')
 # arg : Rien pour le nom par défaut, comment='txt' pour ajouter un com à la fin
 
 # Charge le fichier de configurations
