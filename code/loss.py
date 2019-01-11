41f0c9a7cf2952bee3becee29a3f4844258ef595
GaetanDesrues <gaetan.desrues@free.fr>
2019-01-11 19:30:21 +0100

    ok

--- code/loss.py
+++ code/loss.py
@@ -124,7 +124,7 @@ def dice_loss2(true, logits, eps=1e-7):
         true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
         probas = F.softmax(logits, dim=1)
     dims = (0,) + tuple(range(2, true.ndimension()))
-    intersection = torch.sum((probas * true_1_hot).cuda(), dims)
+    intersection = torch.cuda.sum(probas * true_1_hot, dims)
     cardinality = torch.sum(probas + true_1_hot, dims)
     dice_loss = (2. * intersection / (cardinality + eps)).mean()
     return (1 - dice_loss)
