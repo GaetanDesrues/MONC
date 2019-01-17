2243c759e54cc59901234a30a8f35c1731b704d6
Steven <lampe1steven@gmail.com>
2019-01-17 09:38:02 +0100

    09..37.17.01.19

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ from PIL import Image, ImageOps
 options = OptionCompilation()
 
 # TensorBoardX pour les visualisations
-writer = SummaryWriter('output/runs/late-10')
+writer = SummaryWriter('output/runs/inria-10')
 
 # Charge le fichier de configurations
 config = configparser.ConfigParser()
