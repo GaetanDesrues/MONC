3e79d7ad8c356333c87bc17d6f9df38a44adce15
Corisande Lamy <corisande.lamy@inria.fr>
2019-01-17 12:24:20 +0100

    ok

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ import torch.nn.functional as F
 import torchvision.utils as vutils
 import torchvision.transforms as transforms
 from PIL import Image, ImageOps
-import random
+from random import shuffle
+from random import uniform
 
 
 #Lecture des options shell
